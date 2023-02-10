7cbcfcef847f19d4e9f5aba3a4da89ed378e4b88
Lyden CHAI <74000032+lydenchai@users.noreply.github.com>
2023-02-10 10:22:37 +0700

    format code

--- 05-06-2022/Second/CORRECTION/src/Shape.ts
+++ 05-06-2022/Second/CORRECTION/src/Shape.ts
@@ -1,11 +1,11 @@
-export abstract class Shape{
-    protected leftX: number
-    protected bottomY: number
-    constructor(leftx: number, bottomy: number) {
-        this.leftX = leftx
-        this.bottomY=bottomy
-    }
-    abstract getWidth(): number;
-    abstract getHeight(): number;
-    abstract getArea(): number;
+export abstract class Shape {
+  protected leftX: number;
+  protected bottomY: number;
+  constructor(leftx: number, bottomy: number) {
+    this.leftX = leftx;
+    this.bottomY = bottomy;
+  }
+  abstract getWidth(): number;
+  abstract getHeight(): number;
+  abstract getArea(): number;
 }
--- 05-06-2022/Second/CORRECTION/src/Square.ts
+++ 05-06-2022/Second/CORRECTION/src/Square.ts
@@ -1,18 +1,18 @@
-import { Shape } from './Shape';
+import { Shape } from "./Shape";
 
-export class Square extends Shape{
-    private size: number;
-    constructor(leftx: number, bottomy: number, size: number) {
-        super(leftx, bottomy);
-        this.size = size;
-    }
-    getWidth(): number{
-        return this.size;
-    }
-    getHeight(): number{
-        return this.size;
-    }
-    getArea(): number{
-        return this.size * this.size;
-    }
+export class Square extends Shape {
+  private size: number;
+  constructor(leftx: number, bottomy: number, size: number) {
+    super(leftx, bottomy);
+    this.size = size;
+  }
+  getWidth(): number {
+    return this.size;
+  }
+  getHeight(): number {
+    return this.size;
+  }
+  getArea(): number {
+    return this.size * this.size;
+  }
 }
